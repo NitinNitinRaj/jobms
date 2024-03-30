de8145b0ac017049552d9cf3f99be92873472c2d
bnitinraj <bnitinraj7@gmail.com>
2024-03-30 16:05:57 +0530

    profile for docker

--- src/main/java/org/nr/jobms/job/clients/CompanyClient.java
+++ src/main/java/org/nr/jobms/job/clients/CompanyClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "COMPANY-SERVICE") //load balancing automatically
+@FeignClient(name = "COMPANY-SERVICE", url = "${company-service.url}") //load balancing automatically
 public interface CompanyClient {
 
     @GetMapping("/companies/{id}")
